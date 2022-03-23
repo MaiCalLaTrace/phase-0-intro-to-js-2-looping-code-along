fcb3a771bface2451f7e1cab88405a00ebdb58ef
maicallatrace <maicallatrace@gmail.com>
2022-03-22 19:52:14 -0600

    REPO

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+let names = ['Guadalupe', 'Ollie', 'Aki'];
+
+
+function writeCards(names) {
+    let cards = []
+    for (let i = 0; i < names.length; i++){
+        let message =(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        cards.push(message)
+    }
+        return (cards)
+    
+}
+
+function countDown() {
+    let i = 10;
+    while (i >=0)
+    {
+        console.log(i--);
+    }
+}
